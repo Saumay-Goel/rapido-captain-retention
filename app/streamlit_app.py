6f8159e6b9d3912a986611304bc916588c6c83ae
Saumay.G <sg0627@srmist.edu.in>
2026-05-07 14:55:58 +0530

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -4,6 +4,7 @@ import numpy as np
 import joblib
 import plotly.express as px
 import plotly.graph_objects as go
+import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # ================================================
